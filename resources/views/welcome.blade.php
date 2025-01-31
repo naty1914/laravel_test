075cf56fb1af1a9b98f6ae1bb36aee80c0844f4b
naty1914 <100159348+naty1914@users.noreply.github.com>
2025-01-31 17:16:14 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ninja Network</title>
+    <title>Nati Network</title>
     @vite('resources/css/app.css')
 </head>
 <body class="text-center px-8  py-12">
